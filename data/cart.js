3c23ae80fe7c43530bc1651e12c2da0cd6b39145
fariq <fariqmuhammadkaajayata@gmail.com>
2025-08-17 16:46:36 +0700

    save data and generate HTML for delivery option

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,17 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
+if (!cart) {
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+    }];
+}
 
-[{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
 
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
@@ -26,9 +30,11 @@ export function addToCart(productId) {
     } else {
         cart.push({
             productId: productId,
-            quantity: 1
+            quantity: 1,
+            deliveryOptionId: '1'
         });
     }
+
     saveToStorage();
 }
 
@@ -42,5 +48,6 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
+
     saveToStorage();
 }
